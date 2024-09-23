8d991fcd93ca28b8eda17562166e6449e19c4441
mcadenas4 <mcadenas4@iespompeufabra.cat>
2024-09-23 13:38:01 +0200

    Update sketch.js

--- ocells/sketch.js
+++ ocells/sketch.js
@@ -1,46 +1,37 @@
 function setup() {
- createCanvas(480, 120);
+ createCanvas(400,120)
 }
 
 function draw() {
-  background(204);
+  background(204)
  
- //Ocell esquerra
- beginShape();
- vertex(50,120);
- vertex(100,90);
- vertex(110,60);
- vertex(80,20);
- vertex(210,60);
- vertex(160,80);                                      
- vertex(200,90);
- vertex(140,100);
- vertex(130,120);
- endShape();
- fill(0);
- ellipse(155,60,8,8);
- 
- //Ocell dreta
- beginShape();
- vertex(370,120);
+  //Ocell esquerra
+  beginShape();
+  vertex(50,120); 
+  vertex(100,90);
+  vertex(110,60);
+  vertex(80,20);
+  vertex(210,60);
+  vertex(160,80);
+  vertex(200,90);
+  vertex(140,100);
+  vertex(130,120);
+  endShape();
+  fill(0);
+  ellipse(155,60,8,8);
+
+  //Ocell dreta
+  beginShape();
+  vertex(370,120); 
   vertex(360,90);
-  vertex(290,80);
-  vertex(340,70);
-  vertex(280,50);
-  vertex(420,10);
-  vertex(390,50);
-  vertex(410,90);
+  vertex(290,60);
+  vertex(340,20);
+  vertex(280,60);
+  vertex(340,80);
+  vertex(280,90);
+  vertex(420,100);
   vertex(460,120);
- endShape();
- fill(0);
- ellipse(345,50,10,10);
-
-
-
-
-
-
-
-
-
+  endShape();
+  fill(0);
+  ellipse(155,60,8,8);
 }
