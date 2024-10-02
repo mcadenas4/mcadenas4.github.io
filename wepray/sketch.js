ab823a2e7c5548fc004a821dd0e369dcd93f4b43
mcadenas4 <mcadenas4@iespompeufabra.cat>
2024-10-02 11:13:02 +0200

    Update sketch.js

--- wepray/sketch.js
+++ wepray/sketch.js
@@ -1,1 +1,29 @@
+function setup() {
+  createCanvas(600, 600);
+}
 
+function draw() {
+  background(220);
+  fill(247, 186, 124)
+beginShape();
+vertex(6+h, 501.1875);
+vertex(162+h, 316.1875);
+vertex(227+h, 119.1875);
+vertex(238+h, 113.1875);
+vertex(254+h, 119.1875);
+vertex(254+h, 339.1875);
+vertex(254+h, 118.1875);
+vertex(272+h, 110.1875);
+vertex(288+h, 119.1875);
+vertex(348+h, 313.1875);
+vertex(501+h, 503.1875);
+vertex(368+h, 504.1875);
+vertex(291+h, 362.1875);
+vertex(267+h, 353.1875);
+vertex(255+h, 339.1875);
+vertex(242+h, 354.1875);
+vertex(220+h, 362.1875);
+vertex(144+h, 503.1875);
+vertex(4+h, 501.1875);
+endShape();
+}
