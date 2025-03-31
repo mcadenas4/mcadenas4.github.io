ea3e0ebea8503af76f3aa25fb1207cc79a268979
mcadenas4 <mcadenas4@iespompeufabra.cat>
2025-03-31 13:52:58 +0200

    Update sketch.js

--- cara/cara5/sketch.js
+++ cara/cara5/sketch.js
@@ -31,22 +31,39 @@ function draw() {
 
   // Ojos
   fill(255, 255, 255);
-  ellipse(faceX - 45, faceY - 50, 45, leftEyeSize + 5);
-  ellipse(faceX + 55, faceY - 50, 45, rightEyeSize + 5);
+  ellipse(faceX - 45, faceY - 40, 45, leftEyeSize + 5);
+  ellipse(faceX + 55, faceY - 40, 45, rightEyeSize + 5);
 
   // Pupilas
-  fill(59, 131, 189);
-  ellipse(faceX - 45 + xeye, faceY - 45 + yeye, 22, leftEyeSize / 2);
-  ellipse(faceX + 45 + xeye, faceY - 45 + yeye, 22, rightEyeSize / 2);
+  fill(59, 131 + (5 * h), 189);
+  ellipse(faceX - 45 + xeye, faceY - 40 + yeye, 22, leftEyeSize / 2);
+  ellipse(faceX + 55 + xeye, faceY - 40 + yeye, 22, rightEyeSize / 2);
 
   // Nariz
   fill(120, 70, 50);
   triangle(faceX - 10, faceY, faceX + 10, faceY, faceX, faceY + 30);
 
   // Boca
-  fill(0, 0, 0);
+  fill(0 + (h * 5), 0, 0);
   arc(faceX, faceY + 60, 140, 40 + h, 0, PI);
 
+  //Ceja
+  noFill();
+  stroke(0);
+  strokeWeight(5);
+  arc(faceX + 5, faceY -55, 155, 20 + h, PI, 0);
+  
+  //Sombrero
+  fill(165, 32, 25)
+  noStroke();
+  arc(faceX, faceY - 100, 180, 120, PI, 0)
+  fill(255, 255, 255)
+  arc(faceX, faceY - 100, 180, 50, 0, PI)
+  noFill();
+  stroke(0);
+  strokeWeight(5);
+  arc(faceX, faceY - 100, 180, 120, PI, 0);
+  
   // Mosca
   fill(255);
   noStroke();
